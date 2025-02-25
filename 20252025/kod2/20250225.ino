73f08cb36d1bc893d0e9560eb3f35929ab120eba
iremnuresme <iremesme5@gmail.com>
2025-02-25 14:57:43 +0300

    Add files via upload

---
+++ 20252025/kod2/20250225.ino
@@ -0,0 +1,19 @@
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(3,OUTPUT);
+} 
+
+
+void loop()
+{
+  int pot = analogRead (A0);
+  int LED = map (pot,0,1023,0,255);
+  analogWrite(3,LED);
+  Serial.print("Pot Değeri =");
+  Serial.print(pot);
+  Serial.print("\t LED Değeri =");
+  Serial.print(LED);
+  delay(2);
+}
+ 
